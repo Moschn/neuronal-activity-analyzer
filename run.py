cd5164c6cc242ec7f043ca492385a2b4378cefd2
Moritz Schneider <scmoritz@student.ethz.ch>
2016-02-24 17:41:33 +0100

    Added locate class

--- run.py
+++ run.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
-
 import analyzer
 from matplotlib import pyplot
-
+import matplotlib.cm as cm
 
 loader = analyzer.Loader.open('test/testfile.tif')
 data = loader.next_frame()
 print(data)
-pyplot.figure(1)
-pyplot.imshow(data, interpolation='nearest')
+print(data.shape)
+# pyplot.figure(1)
+pyplot.imshow(data,  cmap=cm.Greys_r)
 pyplot.show()
