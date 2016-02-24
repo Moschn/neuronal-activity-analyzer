599234b34cf9a862067af79b461fa359ca16266e
Conrad Burchert <bconrad@student.ethz.ch>
2016-02-24 13:43:07 +0100

    Added loader base class

---
+++ analyzer/loader.py
@@ -0,0 +1,47 @@
+""" Base class for a Loader, which produces frames """
+
+
+class Loader(object):
+    """ Base class for a file loader. Implementations must overwrite
+    next_frame, can_open and __init__. """
+
+    def __init__(self, path):
+        """Constructor, which opens the specified path in
+        implementations"""
+
+        raise NotImplemented
+
+    @classmethod
+    def open(cls, path, type=None):
+        """ Open a video from a path(file/directory)
+        This will try to choose the correct implementation
+
+        @param path: The path to the video file
+        @param type: Which implementation to use. If None the format is
+                     guessed. More formats can be implemented by
+                     subclassing the Loader class.
+        @return: A Loader object which can output frames"""
+
+        if not type:
+            for c in cls.__subclasses__():
+                if c.can_open(path):
+                    type = c
+
+        return c(path)
+
+    @classmethod
+    def can_open(cls, path):
+        """ Return if the implementation can parse data at the
+        specified path"""
+
+        raise NotImplemented
+
+    def next_frame(self):
+        """ Overwriten in implementation
+
+        @returns: numpy array [height][width] containing pixel data
+
+        @throws: EndOfFile if no more frames are available
+        """
+
+        raise NotImplemented
