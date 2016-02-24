028d270ac488df53ea29c427fdce4d43dc7a773b
Conrad Burchert <bconrad@student.ethz.ch>
2016-02-24 13:01:09 +0100

    Add basic files

---
+++ README.md
@@ -0,0 +1,3 @@
+# Neuronal Network Analyzer
+
+Tool to analyze a video of neural activity and extract activity timeline
---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='neuronal-network-analyzer',
+      description='Neuronal network analyzer',
+      author='Conrad Burchert and Moritz Schneider',
+      author_email='bconrad@student.ethz.ch, scmoritz@student.ethz.ch',
+      py_modules='analyzer'
+     )
