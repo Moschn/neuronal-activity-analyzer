949c6cbddeb74d00bf3317783d001e3690223b1b
Moritz Schneider <scmoritz@student.ethz.ch>
2016-04-13 17:58:00 +0200

    bioformat_loader improved. Does close the open files now

--- analyzer/bioformat_loader.py
+++ analyzer/bioformat_loader.py
@@ -4,18 +4,20 @@ from analyzer.pillow_loader import PILLoader
 from subprocess import check_call
 import os.path
 
+
 class BioFormatLoader(PILLoader):
 
     def __init__(self, path):
         tifpath = path + '.tif'
-        if not os.path.isfile(tifpath): 
+        if not os.path.isfile(tifpath):
             # Convert to tif
             check_call(['./lib/bftools/bfconvert', path, tifpath])
         PILLoader.__init__(self, tifpath)
 
     @classmethod
     def can_open(cls, path):
-        magic_bytes = open(path, 'rb').read(4)
+        with open(path, 'rb') as f:
+            magic_bytes = f.read(4)
         if magic_bytes == b'\xd0\xcf\x11\xe0':
             # Simple PCI (cxd) magic bytes
             return True
