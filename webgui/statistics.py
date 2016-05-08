b85a091a22f89bbeddd5bd5d072dfee1bb9d36ed
Conrad Burchert <bconrad@student.ethz.ch>
2016-05-08 17:45:01 +0200

    Fixed import bug

--- webgui/statistics.py
+++ webgui/statistics.py
@@ -6,7 +6,7 @@ from math import floor
 from .util import run_load, run_save
 import analyzer
 import analyzer.integrator_sum
-import matplotlib.pyplot as mpld3
+import mpld3
 
 statistics = Blueprint('statistics', __name__,
                        template_folder='templates')
