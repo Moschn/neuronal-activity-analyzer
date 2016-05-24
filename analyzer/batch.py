641bf3c8f51e520cd4fd72f7538869f7fb5b1600
Conrad Burchert <conrad.burchert@googlemail.com>
2016-05-24 02:04:53 +0200

    Moved batch logic to analyer

---
+++ analyzer/batch.py
@@ -0,0 +1,42 @@
+import analyzer
+import analyzer.filters
+import analyzer.segmentation
+import analyzer.util
+import analyzer.plot
+import os
+from analyzer.integrator_sum import Integrator_sum
+
+
+def analyze_file(filename, directory, config):
+    videoname = os.path.join(directory, filename)
+    analysis_folder = os.path.join(directory, filename + "-analysis")
+
+    print("analyzing file {}/{}".format(directory, filename))
+    loader = analyzer.loader.open(videoname)
+    time_frame = loader.exposure_time
+    pixel_per_um = loader.pixel_per_um
+
+    print("\tfinding neurons...")
+    segmented = analyzer.segment(loader, config)
+    roi = segmented['segmented']
+    frame = segmented['source']
+
+    sum_roi = Integrator_sum(roi)
+    print("\tanalyzing all frames...")
+    activities = sum_roi.process_parallel_frames(loader)
+
+    print("\tdetecting spikes...")
+    summary_peaks = []
+
+    spikes = analyzer.detect_spikes(activities, config)
+    print("\tplotting results...")
+
+    analyzer.save_results(roi, frame, pixel_per_um, time_frame,
+                          activities.T, spikes, 1, videoname, analysis_folder)
+
+
+def analyze_all_in_folder(path, config):
+    for root, dirs, files in os.walk(path):
+        for name in files:
+            if os.stat('{}/{}'.format(root, name)).st_size > 100000000:
+                analyze_file(name, root, config)
--- batch.py
+++ batch.py
@@ -1,50 +1,14 @@
 #!/usr/bin/env python3
-import analyzer
-import analyzer.filters
-import analyzer.segmentation
 import analyzer.util
-import analyzer.plot
-import os
+from analyzer.batch import analyze_all_in_folder
 from sys import argv
-from analyzer.integrator_sum import Integrator_sum
-
 
 # shutter time in s
 # time_frame = 0.03
 
 
-def analyze_file(filename, directory):
-    videoname = os.path.join(directory, filename)
-    analysis_folder = os.path.join(directory, filename + "-analysis")
-
-    print("analyzing file {}/{}".format(directory, filename))
-    loader = analyzer.loader.open(videoname)
-    time_frame = loader.exposure_time
-    pixel_per_um = loader.pixel_per_um
-
-    print("\tfinding neurons...")
-    segmented = analyzer.segment(loader, config)
-    roi = segmented['segmented']
-    frame = segmented['source']
-
-    sum_roi = Integrator_sum(roi)
-    print("\tanalyzing all frames...")
-    activities = sum_roi.process_parallel_frames(loader)
-
-    print("\tdetecting spikes...")
-    summary_peaks = []
-
-    spikes = analyzer.detect_spikes(activities, config)
-    print("\tplotting results...")
-
-    analyzer.save_results(roi, frame, pixel_per_um, time_frame,
-                          activities.T, spikes, 1, videoname, analysis_folder)
-
-
 config = analyzer.util.load_config('config.py')
 
 path = argv[1]
-for root, dirs, files in os.walk(path):
-    for name in files:
-        if os.stat('{}/{}'.format(root, name)).st_size > 100000000:
-            analyze_file(name, root)
+
+analyze_all_in_folder(path, config)
