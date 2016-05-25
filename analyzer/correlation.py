3fa0b294650bab0e5292b033737d3e9167f582e2
Conrad Burchert <bconrad@student.ethz.ch>
2016-05-25 12:52:01 +0200

    Correlations now contain negative delays

--- analyzer/correlation.py
+++ analyzer/correlation.py
@@ -16,20 +16,22 @@ def correlate_activities(activities, exposure_time):
         numpy array[num_neurons, num_neurons, n_frames]
         corellation of the activity of the first neuron with that of the second
         neuron, if the activity of the first neuron is shifted backwards by
-        the number of frames specified in the third argument"""
+        the number of frames specified in the third argument.
+        Normalized to 0 to 1"""
 
     n_neurons = activities.shape[0]
     n_frames = activities.shape[1]
     n_shifts = round(CORRELATION_MAX_SHIFT / exposure_time)
+    min_index = n_frames // 2 - n_shifts
+    max_index = n_frames // 2 + n_shifts
 
-    correlations = numpy.ndarray((n_neurons, n_neurons, n_shifts),
+    correlations = numpy.ndarray((n_neurons, n_neurons, 2 * n_shifts),
                                  dtype='float')
 
     def calc_one_correlation(i, j):
         for k in range(0, n_shifts):
             correlated = numpy.correlate(activities[j], activities[i], 'same')
-            correlations[i, j, :] = (
-                correlated[n_frames // 2: n_frames // 2 + n_shifts])
+            correlations[i, j, :] = correlated[min_index: max_index] / n_frames
 
     with ThreadPoolExecutor(max_workers=THREAD_COUNT) as executor:
         for i in range(0, n_neurons):
