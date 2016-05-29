Fixed error in correlation calculation slowing down calculation and improved complexity

analyzer/correlation.py
@@ -1,4 +1,5 @@
 import numpy
+import time
 from concurrent.futures import ThreadPoolExecutor
 
 from .settings import THREAD_COUNT, CORRELATION_MAX_SHIFT
@@ -22,16 +23,22 @@ def correlate_activities(activities, exposure_time):
     n_neurons = activities.shape[0]
     n_frames = activities.shape[1]
     n_shifts = round(CORRELATION_MAX_SHIFT / exposure_time)
-    min_index = n_frames // 2 - n_shifts
-    max_index = n_frames // 2 + n_shifts
 
-    correlations = numpy.ndarray((n_neurons, n_neurons, 2 * n_shifts),
+    correlations = numpy.ndarray((n_neurons, n_neurons, 2 * n_shifts + 1),
                                  dtype='float')
 
     def calc_one_correlation(i, j):
-        for k in range(0, n_shifts):
-            correlated = numpy.correlate(activities[j], activities[i], 'same')
-            correlations[i, j, :] = correlated[min_index: max_index] / n_frames
+        # To only retrive correlations with time shifts smaller than
+        # n_shifts we pad one of the arrays with zeros on both sides
+        # and used numpys 'valid' mode, which only calculates correlations
+        # where the arrays overlap completely
+        # see https://docs.scipy.org/doc/numpy-1.10.0/reference/generated/
+        # numpy.convolve.html#numpy.convolve
+        act1 = activities[j]
+        # zeropads the array to both sides with n_shifts zeros
+        act2 = numpy.pad(activities[i], (n_shifts, n_shifts), 'constant')
+        correlated = numpy.correlate(act1, act2, 'valid')
+        correlations[i, j, :] = correlated / n_frames  # normalize
 
     with ThreadPoolExecutor(max_workers=THREAD_COUNT) as executor:
         for i in range(0, n_neurons):