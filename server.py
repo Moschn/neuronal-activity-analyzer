f36ef5f57ecfa0a9e4700991077befefc7d87dc6
Moritz Schneider <scmoritz@student.ethz.ch>
2016-06-21 17:50:53 +0200

    Added gevent server for deployment

---
+++ server.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from gevent.wsgi import WSGIServer
+from webgui.bootstrap import create_app
+
+if __name__ == '__main__':
+    app = create_app()
+    http_server = WSGIServer(('', 16290), app)
+    http_server.serve_forever()
