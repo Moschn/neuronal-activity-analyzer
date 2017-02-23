94f7c4dab1ad212a8aaf37f087c648210c802044
mathiasaebersold <mathias.aebersold@gmail.com>
2017-02-23 11:43:09 +0100

    Added option to disable file upload in webgui

--- webconfig.py
+++ webconfig.py
@@ -4,7 +4,7 @@ ROOT_DIR = abspath(dirname(__file__))
 
 VIDEO_FOLDER = join(ROOT_DIR, 'uploads')
 UPLOAD_FOLDER = join(VIDEO_FOLDER, 'upload')
-DATA_FOLDER = join(ROOT_DIR, 'data')
+DATA_FOLDER = join(ROOT_DIR, '/home/mathias/NeuroAnalyzer/TestDataMovie/')
 
 UPLOAD_MAX_SIZE = 200000000000  # 200gb
 
@@ -12,3 +12,6 @@ SECRET_KEY = '\x96\xabV\x04\xf0\xfb\x1eii\xf7kU8Jj:\xc3\x95\xfbt\xc9x\xa8\xc8'
 
 # Hide batch and save to NAS buttons
 DEMO_MODE = False
+
+# Disable file upload
+UPLOAD_ENABLE = False
